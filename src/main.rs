4812a8924fc797a1143bb3c20324f1c76dde6e60
Tronje Krabbe <hi@tron.je>
2019-02-08 12:52:26 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,51 @@
+use clap::{App, Arg};
+
+fn annoyify_random(s: &str) -> String {
+    let out = s.to_lowercase();
+
+    out.chars()
+        .map(|mut c| {
+            if rand::random() {
+                c.make_ascii_uppercase();
+            }
+            c
+        })
+        .collect()
+}
+
+fn annoyify_alternating(s: &str) -> String {
+    let out = s.to_lowercase();
+    let mut last_upcase = false;
+
+    out.chars()
+        .map(|mut c| {
+            if last_upcase {
+                last_upcase = false;
+            } else {
+                c.make_ascii_uppercase();
+                last_upcase = true;
+            }
+            c
+        })
+        .collect()
+}
+
+fn main() {
+    let matches = App::new("annoyify")
+        .version(env!("CARGO_PKG_VERSION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .about("Annoyify a phrase")
+        .arg(Arg::with_name("INPUT"))
+        .arg(Arg::with_name("random").short("r").long("random"))
+        .get_matches();
+
+    let input = matches
+        .value_of("INPUT")
+        .unwrap_or("you need to provide an input");
+
+    if matches.is_present("random") {
+        println!("{}", annoyify_random(input));
+    } else {
+        println!("{}", annoyify_alternating(input));
+    }
+}
