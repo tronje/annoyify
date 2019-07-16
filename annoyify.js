6bfd2858a755508ba8244523298ee53956a41f8e
Tronje Krabbe <hi@tron.je>
2019-07-16 15:51:01 +0200

    Add a sick button animation

--- annoyify.js
+++ annoyify.js
@@ -22,3 +22,22 @@ function run() {
 
 	document.getElementById("output_text").value = annoyified;
 }
+
+function animated_button_response(e) {
+	e.preventDefault;
+
+	//reset animation
+	e.target.classList.remove('animate');
+
+	e.target.classList.add('animate');
+	setTimeout(function() {
+		e.target.classList.remove('animate');
+	}, 700);
+
+	run();
+};
+
+window.addEventListener('DOMContentLoaded', (event) => {
+	let button = document.getElementById("animated_button");
+	button.addEventListener('click', animated_button_response, false);
+});
